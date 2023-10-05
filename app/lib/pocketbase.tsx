93fae715d50eb5c4a924b42a9ed41a0014210612
yurii-krymskiy <yurii.krymskyi@gmail.com>
2023-10-05 13:54:52 +0200

    sss

--- app/lib/pocketbase.tsx
+++ app/lib/pocketbase.tsx
@@ -1,8 +1,7 @@
 import PocketBase from "pocketbase";
 
-
 export const pocketbase = () => {
-  const pb = new PocketBase("http://127.0.0.1:8090/api/collections/posts/records");
+  const pb = new PocketBase("http://127.0.0.1:8090/");
 
   pb.autoCancellation(false);
 

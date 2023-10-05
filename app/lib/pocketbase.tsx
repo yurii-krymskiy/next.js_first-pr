1da359d529b0eef957d77370ac9b83c581a3f72c
yurii-krymskiy <yurii.krymskyi@gmail.com>
2023-10-05 14:34:49 +0200

    sss

--- app/lib/pocketbase.tsx
+++ app/lib/pocketbase.tsx
@@ -2,7 +2,7 @@ import PocketBase from "pocketbase";
 
 export const pocketbase = () => {
   const pb = new PocketBase(
-    "http://127.0.0.1:8090/api/collections/posts/records"
+    "http://127.0.0.1:8090/"
   );
 
   pb.autoCancellation(false);
@@ -14,21 +14,5 @@ export const pocketbase = () => {
   return {
     getPosts,
   };
-};
-
-const url = "http://127.0.0.1:8090/api/collections/posts/records";
-
-export const getData = async () => {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Статус: ${response.status}`);
-    }
-    const data = await response.json();
-    return data; // Повертаємо дані у вигляді масиву
-  } catch (error) {
-    console.error("Виникла помилка:", error);
-    throw error;
-  }
 };
 
